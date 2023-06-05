edd4dc0c73e3d4e7f42ad4bd4f12e07ccca4c608
mhmdfrhn <frhn2707@gmail.com>
2023-06-05 19:58:19 +0700

    add searchItem component

---
+++ client/src/components/searchItem/SearchItem.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./searchItem.css";
+
+const SearchItem = () => {
+  return (
+    <div>
+      <h1>SearchItem</h1>
+    </div>
+  );
+};
+
+export default SearchItem;
