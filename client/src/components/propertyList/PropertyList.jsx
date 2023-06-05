05d5ca8c4e0bc4331607a1e353f6532146155fc0
mhmdfrhn <frhn2707@gmail.com>
2023-06-05 19:58:03 +0700

    add propertyList component

---
+++ client/src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./propertyList.css";
+
+const PropertyList = () => {
+  return (
+    <div>
+      <h1>PropertyList</h1>
+    </div>
+  );
+};
+
+export default PropertyList;
