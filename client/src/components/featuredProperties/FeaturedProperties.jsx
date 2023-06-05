aea8b809380c021b30ba8e9582483f4d89fb3a00
mhmdfrhn <frhn2707@gmail.com>
2023-06-05 19:56:18 +0700

    add featuredProperties compoenent

---
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./featuredProperties.css";
+
+const FeaturedProperties = () => {
+  return (
+    <div>
+      <h1>FeaturedProperties</h1>
+    </div>
+  );
+};
+
+export default FeaturedProperties;
