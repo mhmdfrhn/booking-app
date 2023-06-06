bbcaac461eeb424485698f7aaad317407baa5386
mhmdfrhn <frhn2707@gmail.com>
2023-06-06 19:03:30 +0700

    create footer

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -3,8 +3,49 @@ import "./footer.css";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="footer">
+      <div className="footer-lists">
+        <ul className="footer-list">
+          <li className="footer-list_item">Countries</li>
+          <li className="footer-list_item">Regions</li>
+          <li className="footer-list_item">Cities</li>
+          <li className="footer-list_item">Districts</li>
+          <li className="footer-list_item">Airports</li>
+          <li className="footer-list_item">Hotels</li>
+        </ul>
+        <ul className="footer-list">
+          <li className="footer-list_item">Homes </li>
+          <li className="footer-list_item">Apartments </li>
+          <li className="footer-list_item">Resorts </li>
+          <li className="footer-list_item">Villas</li>
+          <li className="footer-list_item">Hostels</li>
+          <li className="footer-list_item">Guest houses</li>
+        </ul>
+        <ul className="footer-list">
+          <li className="footer-list_item">Unique places to stay </li>
+          <li className="footer-list_item">Reviews</li>
+          <li className="footer-list_item">Unpacked: Travel articles </li>
+          <li className="footer-list_item">Travel communities </li>
+          <li className="footer-list_item">Seasonal and holiday deals </li>
+        </ul>
+        <ul className="footer-list">
+          <li className="footer-list_item">Car rental </li>
+          <li className="footer-list_item">Flight Finder</li>
+          <li className="footer-list_item">Restaurant reservations </li>
+          <li className="footer-list_item">Travel Agents </li>
+        </ul>
+        <ul className="footer-list">
+          <li className="footer-list_item">Curtomer Service</li>
+          <li className="footer-list_item">Partner Help</li>
+          <li className="footer-list_item">Careers</li>
+          <li className="footer-list_item">Sustainability</li>
+          <li className="footer-list_item">Press center</li>
+          <li className="footer-list_item">Safety Resource Center</li>
+          <li className="footer-list_item">Investor relations</li>
+          <li className="footer-list_item">Terms & conditions</li>
+        </ul>
+      </div>
+      <div className="footer-text">Copyright © 2023 HBooking.</div>
     </div>
   );
 };
