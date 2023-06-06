16663c3ebdc3cd004f457132ce4c02e031165b81
mhmdfrhn <frhn2707@gmail.com>
2023-06-06 19:05:20 +0700

    slicing mail section

--- client/src/components/mailList/MailList.jsx
+++ client/src/components/mailList/MailList.jsx
@@ -3,8 +3,15 @@ import "./mailList.css";
 
 const MailList = () => {
   return (
-    <div>
-      <h1>MailList</h1>
+    <div className="mail">
+      <h1 className="mail-title">Save time, save money!</h1>
+      <span className="mail-desc">
+        Sign up and we'll send the best deals to you
+      </span>
+      <div className="mail-input_container">
+        <input type="text" placeholder="Your Email" />
+        <button>Subscribe</button>
+      </div>
     </div>
   );
 };
