910557fc168b41a875753ef1ff3279604a4ffe83
mhmdfrhn <frhn2707@gmail.com>
2023-06-06 19:06:02 +0700

    final slicing propertyList section

--- client/src/components/propertyList/PropertyList.jsx
+++ client/src/components/propertyList/PropertyList.jsx
@@ -3,8 +3,40 @@ import "./propertyList.css";
 
 const PropertyList = () => {
   return (
-    <div>
-      <h1>PropertyList</h1>
+    <div className="property-list">
+      <div className="property-list_item">
+        <img
+          src="https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o="
+          alt="img1"
+          className="property_item-img"
+        />
+        <div className="property-list_titles">
+          <h1>Hotels</h1>
+          <h2>233 Hotels</h2>
+        </div>
+      </div>
+      <div className="property-list_item">
+        <img
+          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg"
+          alt="img1"
+          className="property_item-img"
+        />
+        <div className="property-list_titles">
+          <h1>Resorts</h1>
+          <h2>2331 Hotels</h2>
+        </div>
+      </div>
+      <div className="property-list_item">
+        <img
+          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg"
+          alt="img1"
+          className="property_item-img"
+        />
+        <div className="property-list_titles">
+          <h1>Cabins</h1>
+          <h2>2335 Hotels</h2>
+        </div>
+      </div>
     </div>
   );
 };
