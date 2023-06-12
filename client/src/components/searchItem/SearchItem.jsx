803f28e3b5d3779e32ab0db87c454d318a6aa33b
mhmdfrhn <frhn2707@gmail.com>
2023-06-12 11:46:59 +0700

    add search item index

--- client/src/components/searchItem/SearchItem.jsx
+++ client/src/components/searchItem/SearchItem.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import "./searchItem.css";
 
-const SearchItem = () => {
+const SearchItem = ({ item }) => {
   return (
-    <div>
-      <h1>SearchItem</h1>
+    <div className="search-item">
+      <img src={item.photos[0]} alt="" className="item_img" />
+      <div className="item_desc">
+        <h1 className="item_title">{item.name}</h1>
+        <span className="item_distance">{item.distance}m from center</span>
+        <span className="item_taxi-op">Free airport taxi</span>
+        <span className="item_subtitle">
+          Studio Apartment with Air conditioning
+        </span>
+        <span className="item_features">{item.desc}</span>
+        <span className="item_cancel-op">Free cancellation </span>
+        <span className="cancel-op_subtitle">
+          You can cancel later, so lock in this great price today!
+        </span>
+      </div>
+      <div className="item_details">
+        {item.rating && (
+          <div className="item_rating">
+            <span>Excellent</span>
+            <button>{item.rating}</button>
+          </div>
+        )}
+        <div className="item_detail-text">
+          <span className="item_price">${item.cheapestPrice}</span>
+          <span className="item_tax-op">Includes taxes and fees</span>
+          <Link to={`/hotels/find/${item._id}`}>
+            <button className="item_check-btn">See availability</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
