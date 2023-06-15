2f8bf7ac134b8a8442a9de1197be8f16384b9609
mhmdfrhn <frhn2707@gmail.com>
2023-06-15 20:42:40 +0700

    Docs line of code

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,7 +1,11 @@
+// ** User Controller */
+
 import User from "../models/User.js";
 
+// ** Get User */
 export const getUser = async (req, res, next) => {
   try {
+    //** get user id */
     const user = await User.findById(req.params.id);
     res.status(200).json(user);
   } catch (error) {
@@ -9,8 +13,10 @@ export const getUser = async (req, res, next) => {
   }
 };
 
+// ** Get All User */
 export const getUsers = async (req, res, next) => {
   try {
+    //** get users */
     const users = await User.find();
     res.status(200).json(users);
   } catch (error) {
@@ -18,8 +24,10 @@ export const getUsers = async (req, res, next) => {
   }
 };
 
+// ** Update User */
 export const updateUser = async (req, res, next) => {
   try {
+    //** get user by id and update */
     const updatedUser = await User.findByIdAndUpdate(
       req.params.id,
       {
@@ -33,8 +41,10 @@ export const updateUser = async (req, res, next) => {
   }
 };
 
+// ** Delete User */
 export const deleteUser = async (req, res, next) => {
   try {
+    //** get user id and delete */
     await User.findByIdAndDelete(req.params.id);
     res.status(200).json("User has been deleted!");
   } catch (error) {
