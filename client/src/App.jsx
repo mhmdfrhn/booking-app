ab64294f95ed74536bf676474a41e16d03e41c86
mhmdfrhn <frhn2707@gmail.com>
2023-06-17 10:21:52 +0700

    Docs line of code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+//** Routes */
 import Home from "./pages/home/Home";
 import Hotel from "./pages/hotel/Hotel";
 import List from "./pages/list/List";
